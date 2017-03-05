01fc4f44a3b9b228baa0d995b74490b47c701451
Pavel Gerasimov <gerasimov.pk@gmail.com>
2017-03-05 16:31:38 +0300

    say_hello

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var token = '330844015:AAHwmV2429xiAd95EmpaVWecEz3AYERb02Q';
 
 var Bot = require('node-telegram-bot-api'),
@@ -12,6 +11,13 @@ bot.onText(/^(.+)$/, function (msg, match) {
     console.log('bot on text');
     var name = match[1];
     bot.sendMessage(msg.chat.id, 'You said: ' + name + '!').then(function () {
+        // reply sent!
+    });
+});
+
+bot.onText(/^\/say_hello$/, function (msg, match) {
+    console.log('bot on say_hello$');
+    bot.sendMessage(msg.chat.id, 'Hello dear friend!!!').then(function () {
         // reply sent!
     });
 });
