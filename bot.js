8a7b876368ba89c215e0be8984e782f2c3224ae1
Pavel Gerasimov <gerasimov.pk@gmail.com>
2017-03-05 16:54:09 +0300

    add timer

--- bot.js
+++ bot.js
@@ -5,6 +5,8 @@ var token = '330844015:AAHwmV2429xiAd95EmpaVWecEz3AYERb02Q';
 var Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true });
 
+var interval;
+
 console.log('bot server started...');
 
 bot.onText(/^(.+)$/, function (msg, match) {
@@ -20,4 +22,22 @@ bot.onText(/^\/say_hello$/, function (msg, match) {
     bot.sendMessage(msg.chat.id, 'Hello dear friend!!!').then(function () {
         // reply sent!
     });
+});
+
+bot.onText(/^\/subscribe$/, function (msg, match) {
+    console.log('bot on subscribe');
+    bot.sendMessage(msg.chat.id, 'OK!').then(function () {
+        let interval = setInterval(()=>{
+                bot.sendMessage(msg.chat.id, 'Current time: '+ new Date().toDateString())
+        }, 5000);
+    });
+
+});
+
+bot.onText(/^\/cancel$/, function (msg, match) {
+    console.log('bot on cancel');
+    bot.sendMessage(msg.chat.id, 'OK!').then(function () {
+        clearInterval(interval);
+    });
+
 });
