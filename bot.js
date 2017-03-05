2197ead580dbeafb1a87278f6aa13556eadf57c4
Pavel Gerasimov <gerasimov.pk@gmail.com>
2017-03-05 21:41:26 +0300

    fix

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ bot.onText(/^\/get_logs$/, function (msg, match) {
     db.getLogs((res) => {
         res.map(el => {
             bot.sendMessage(msg.chat.id, 'Current element: ' + JSON.stringify(el))
-            bot.forwardMessage(msg.chat.id, el.chat_id, el.id)
+            bot.forwardMessage(msg.chat.id, el.message.chat_id, el.message.id)
         })
     });
 });
