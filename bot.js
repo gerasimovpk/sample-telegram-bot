7f9c2e861f4d27c499e9d3ce5576519343efb0f0
Pavel Gerasimov <gerasimov.pk@gmail.com>
2017-03-05 16:06:56 +0300

    heroku example bot

---
+++ bot.js
@@ -0,0 +1,33 @@
+var token = '330844015:AAHwmV2429xiAd95EmpaVWecEz3AYERb02Q';
+
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
+
+console.log('bot server started...');
+
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+    console.log('bot on text');
+    var name = match[1];
+    bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+        // reply sent!
+    });
+});
+
+bot.onText(/^(.+)$/, function (msg, match) {
+    console.log('bot on text');
+    var name = match[1];
+    bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+        // reply sent!
+    });
+});
+
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+    console.log('bot on text');
+    var result = 0;
+    match[1].trim().split(/\s+/).forEach(function (i) {
+        result += (+i || 0);
+    })
+    bot.sendMessage(msg.chat.id, result).then(function () {
+        // reply sent!
+    });
+});
