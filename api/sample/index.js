f352d483de5ca4dd30254f8fc118e75d22953f1c
Pavel Gerasimov <gerasimov.pk@gmail.com>
2017-03-05 15:43:52 +0300

    sample

---
+++ api/sample/index.js
@@ -0,0 +1,3 @@
+function getSampleHelloWorld() {
+    return "sampleHelloWorld"
+}
--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/api/v1/sample', function(request, response) {
+    response.json({ message: 'hooray! welcome to our api!' });
+});
+app.get('/api/v1/sample2', function(request, response) {
+    response.json({ message: apiSample.getSampleHelloWorld() });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
