cb1779dc567d744fbf99d337c72f4c5fa23a3669
Pavel Gerasimov <gerasimov.pk@gmail.com>
2017-03-05 17:06:06 +0300

    fix

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ bot.onText(/^\/say_hello$/, function (msg, match) {
 });
 bot.onText(/^\/send_cake$/, function (msg, match) {
     console.log('bot on send_cake$');
-    bot.sendPhoto(chatId, 'https://pp.userapi.com/c837328/v837328893/12b0a/_Lp_Tt3-Zxk.jpg', {caption: 'Крутой торт, /заказывай!'});
+    bot.sendPhoto(msg.chat.id, 'https://pp.userapi.com/c837328/v837328893/12b0a/_Lp_Tt3-Zxk.jpg', {caption: 'Крутой торт, /заказывай!'});
 });
 bot.onText(/^\/заказывай$/, function (msg, match) {
     bot.sendMessage(msg.chat.id, 'Супер! перезвоню тебе!').then(function () {
