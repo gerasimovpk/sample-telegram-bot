1fcd69c42e1a204534ddd1485c93076c065e2779
Pavel Gerasimov <gerasimov.pk@gmail.com>
2017-03-05 17:36:18 +0300

    try keyboard

--- bot.js
+++ bot.js
@@ -9,13 +9,13 @@ var interval;
 
 console.log('bot server started...');
 
-// bot.onText(/^(.+)$/, function (msg, match) {
-//     console.log('bot on text');
-//     var name = match[1];
-//     bot.sendMessage(msg.chat.id, 'You said: ' + name + '!').then(function () {
-//         // reply sent!
-//     });
-// });
+bot.onText(/^(.+)$/, function (msg, match) {
+    console.log('bot on text');
+    var name = match[1];
+    bot.sendMessage(msg.chat.id, 'You said: ' + name + '!').then(function () {
+        // reply sent!
+    });
+});
 
 bot.onText(/^\/say_hello$/, function (msg, match) {
     console.log('bot on say_hello$');
@@ -28,10 +28,22 @@ bot.onText(/^\/send_cake$/, function (msg, match) {
     bot.sendPhoto(msg.chat.id, 'https://pp.userapi.com/c837328/v837328893/12b0a/_Lp_Tt3-Zxk.jpg', {caption: 'Крутой торт, заказывай! /want_it'});
 });
 bot.onText(/^\/want_it/, function (msg, match) {
+    //reply_markup = telegram.ReplyKeyboardMarkup([[telegram.KeyboardButton('Share contact', request_contact=True)]])
+    //bot.sendMessage(CHAT_ID, 'Example', reply_markup=reply_markup)
+
+    var option = {
+        "parse_mode": "Markdown",
+        "reply_markup": {  "keyboard": [["Yes"],["No"]]  }
+    };
+    bot.sendMessage(msg.chat.id, "*Some* message here.", option);
+
+
     bot.sendMessage(msg.chat.id, 'Супер,' + msg.from.first_name + ', я перезвоню тебе!').then(function () {
         // reply sent!
     });
 });
+
+
 
 bot.onText(/^\/subscribe$/, function (msg, match) {
     console.log('bot on subscribe');
